c4bad25616e0035098bd057ab30d81eb1f271795
fellipe <fellipepf@gmail.com>
2020-10-11 17:12:11 +0100

    Basic R commands

---
+++ R/BasicR.R
@@ -0,0 +1,23 @@
+###
+###Comments
+###
+
+#####Using help
+help.start()
+
+# ?functioname
+?help
+?plot
+help(plot)
+help.search("mean")
+help.search("plot")
+
+
+## attribution
+a = 2
+b = 3
+a+b
+
+a <- 2
+b <- 3
+a+b
