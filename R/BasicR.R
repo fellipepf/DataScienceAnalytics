7412a9c3baa8a1eee508a0efc683220fef9c4f0f
fellipe <fellipepf@gmail.com>
2020-10-17 11:01:10 +0100

    plot functions

--- R/BasicR.R
+++ R/BasicR.R
@@ -2,6 +2,15 @@
 ###Comments
 ###
 
+#clear console
+#Ctrl+L   
+
+#clear enviroment
+rm(list=ls())
+
+#clear graph
+graphics.off()
+
 #####Using help
 help.start()
 
@@ -13,6 +22,10 @@ help.search("mean")
 help.search("plot")
 
 
+dir() #show files in the current directory
+
+install.packages("package name") # install a package from repository
+
 ## attribution
 a = 2
 b = 3
@@ -21,3 +34,9 @@ a+b
 a <- 2
 b <- 3
 a+b
+
+
+x <- c(7,5,3,6,7,5,4,2,1,111,232,445,656,76)
+x
+sort(x)
+order(x) # returns a index vector containing the sorted order of the vector vec
---
+++ R/PlottingFunctions.R
@@ -0,0 +1,20 @@
+
+x <- 1:20
+y <- 2*x^2
+
+
+
+plot(x) # plot of the values of x (on the y-axis) ordered on the x-axis
+plot(x, y) # bivariate plot of x (on the x-axis) and y (on the y-axis)
+hist(x) # histogram of the frequencies of x
+barplot(x) # histogram of the values of x; use horiz=TRUE for horizontal bars
+dotchart(x) # if x is a data frame, plots a Cleveland dot plot (stacked plots line-by-line and column-by-column)
+pie(x) # circular pie-chart
+boxplot(x) # "box-and-whiskers" plot
+
+# id. than plot() but the points with similar coordinates are drawn as
+# owers which petal number represents thenumber of points
+sunflowerplot(x, y) 
+
+
+stripchart(x) # plot of the values of x on a line (an alternative to boxplot() for small sample sizes)
